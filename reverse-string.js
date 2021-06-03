df7a226bb2b90d1ff39828d367d1cb32fcd8d81d
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-06-03 17:11:40 +0545

    reverse string function implemented

---
+++ reverse-string.js
@@ -0,0 +1,14 @@
+// write a function that accepts a string input and returns a reversed copy
+
+inputString = "I love coding.";
+
+var reverseString = (inputString) => {
+  var lengthOfInputString = inputString.length;
+  var reversedString = '';
+  for(var i = 1; i <= lengthOfInputString; i++) {
+    reversedString += inputString[lengthOfInputString - i];
+  }
+  return reversedString;
+}
+
+console.log(reverseString(inputString))
