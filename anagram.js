baa8dd1945d7f1d94106b80d62ed7d5cbcc4df4b
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-06-09 16:05:27 +0545

    other way of solving

--- anagram.js
+++ anagram.js
@@ -1,6 +1,8 @@
-var word = "cinema";
-var word2 = "iceman";
+var word = "qwerty";
+var word2 = "ytrewq";
 
+
+// one way with O(n) time complexity
 var anagram = (word, word2) => {
   if (word.length !== word2.length) {
     return false;
@@ -26,4 +28,24 @@ var anagram = (word, word2) => {
   return true;
 }
 
+//another way with O(n) time complexity
+var anagram2 = (firstWord, secondWord) => {
+  if (firstWord.length !== secondWord.length) return false;
+
+  frequencyCounterForFirstWord = {};
+
+  for (var val of firstWord) {
+    frequencyCounterForFirstWord[val] = (frequencyCounterForFirstWord[val] || 0) + 1; // {c:1, i:1, n:1, e:1, m:1, a:1}
+  }
+
+  for (var val of secondWord) {
+    if (!frequencyCounterForFirstWord[val]) {     //if does not contain or is 0
+      return false;
+    } else {
+      frequencyCounterForFirstWord[val]--;     // substract 1 so that when all letters are done the key with that letter has 0 value
+    }
+  }
+  return true;
+}
 console.log(`${word} & ${word2} are anagrams: ${anagram(word, word2)}`);
+console.log(`${word} & ${word2} are anagrams: ${anagram2(word, word2)}`);
