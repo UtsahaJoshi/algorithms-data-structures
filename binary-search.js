e1c53ab6d77ad32fcb3a881f6a8e2e6cd7f63b77
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-06-13 23:32:37 +0545

    return -1 if not found

--- binary-search.js
+++ binary-search.js
@@ -1,11 +1,14 @@
 var array = [1, 2, 5, 8, 11, 12, 22, 45, 46, 56, 66, 68]; //has to be sorted
-searchValue = 66;
+searchValue = 46;
 
 var binarySearch = (array, searchValue) => {
   var left = 0;
   var right = array.length - 1;
   while (left < right) {
     var mid = Math.floor((left + right) /2);
+    if (mid === left) {
+      return -1;
+    }
     if (searchValue < array[mid]) {
       right = mid;
     } else if (searchValue > array[mid]) {
@@ -14,7 +17,6 @@ var binarySearch = (array, searchValue) => {
       return mid;
     }
   }
-  return -1;
 }
 
 console.log(binarySearch(array, searchValue));
