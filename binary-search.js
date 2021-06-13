ecb3b324b9a82d42d0ec25921d66b8d404d84efc
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-06-13 23:35:59 +0545

    better code

--- binary-search.js
+++ binary-search.js
@@ -1,22 +1,20 @@
 var array = [1, 2, 5, 8, 11, 12, 22, 45, 46, 56, 66, 68]; //has to be sorted
-searchValue = 46;
+searchValue = 8;
 
 var binarySearch = (array, searchValue) => {
   var left = 0;
   var right = array.length - 1;
   while (left < right) {
     var mid = Math.floor((left + right) /2);
-    if (mid === left) {
-      return -1;
-    }
     if (searchValue < array[mid]) {
-      right = mid;
+      right = mid - 1;
     } else if (searchValue > array[mid]) {
-      left = mid;
+      left = mid + 1;
     } else {
       return mid;
     }
   }
+  return -1;
 }
 
 console.log(binarySearch(array, searchValue));
