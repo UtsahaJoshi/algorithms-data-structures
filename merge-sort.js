e47cc1d97b67e2583803bf9c197e6379968d0518
UtsahaJoshi <joshiutsaha1@gmail.com>
2021-06-20 16:00:54 +0545

    merge sort

--- merge-sort.js
+++ merge-sort.js
@@ -1,7 +1,10 @@
+var arr = [2, 10, 25, 6, 4, 12, 34, 50];
+
 var merge = (arr1, arr2) => {
   var array = [];
   var i = 0;
   var j = 0;
+
   while (i < arr1.length && j < arr2.length) {
     if (arr2[j] > arr1[i]) {
       array.push(arr1[i]);
@@ -22,4 +25,13 @@ var merge = (arr1, arr2) => {
   return array;
 }
 
-console.log(merge([1, 10,], [6, 25]));
+
+var mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  var mid = Math.floor(arr.length / 2);
+  var left = mergeSort(arr.slice(0, mid));
+  var right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+console.log(mergeSort(arr));
